e1328d1fa88d903c8bdc5c6482c1afc7506ff044
Gabriel <gabriel.stepien@inf.ufrgs.br>
2018-11-29 20:17:34 -0200

    fix person controller and service. Added travis test

--- src/main/java/com/vrm/controller/PersonController.java
+++ src/main/java/com/vrm/controller/PersonController.java
@@ -1,6 +1,8 @@
 package com.vrm.controller;
 
+import com.vrm.model.Groups;
 import com.vrm.model.Person;
+import com.vrm.model.User;
 import com.vrm.service.PersonService;
 
 import org.springframework.web.bind.annotation.PutMapping;
@@ -10,16 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
 	private PersonService personService = new PersonService();
 
-	public User userLogin(String login, String password) {
+	public User userLogin(String login, String password) throws Exception {
 		User user = this.personService.loginUser(login, password);
 		return user;
 	}
 
-	public void userRegister(String name, String login, String password, int apartmentNumber, int floor, int building, Enum<Groups> group) {
+	public void userRegister(String name, String login, String password, int apartmentNumber, int floor, int building, Groups group) throws Exception {
 		this.personService.registerUser(name, login, password, apartmentNumber, floor, building, group);
 	}
 
-	public void visitorRegister(String name, Enum<Groups> group, User userToVinculate) {
-		this.personService.registerUser(name, group, userToVinculate);
+	public void visitorRegister(String name, Groups group, User userToVinculate) throws Exception{
+		this.personService.registerVisitor(name,group, userToVinculate);
 	}
 }
--- src/main/java/com/vrm/service/PersonService.java
+++ src/main/java/com/vrm/service/PersonService.java
@@ -2,22 +2,25 @@ package com.vrm.service;
 import java.util.ArrayList;
 
 import com.vrm.model.Person;
+import com.vrm.model.User;
 import com.vrm.data.PersonDAO;
 import com.vrm.systemexceptions.InvalidPersonGroupException;
 import com.vrm.model.Groups;
 
 public class PersonService {
 	private PersonDAO personDAO = new PersonDAO();
-	public void registerUser(String name, String login, String password, int apartmentNumber, int floor, int building, Enum<Groups> group) throws Exception {
-		if (group == Enum<Groups>.User || group == Enum<Groups>.Administrator) {
+
+
+	public void registerUser(String name, String login, String password, int apartmentNumber, int floor, int building, Groups group) throws Exception {
+		if (group == Groups.User || group == Groups.Administrator) {
 			this.personDAO.saveUser(name, login, password, apartmentNumber, floor, building, group);
 		}
 
 		throw new InvalidPersonGroupException();
 	}
 
-	public void registerVisitor(String name, Enum<Groups> group, User userToVinculate) throws Exception {
-		if (group == Enum<Groups>.Visitor || group == Enum<Groups>.Maintenance) {
+	public void registerVisitor(String name, Groups group, User userToVinculate) throws Exception {
+		if (group == Groups.Visitor || group == Groups.Maintenance) {
 			this.personDAO.saveVisitor(name, group, userToVinculate);
 		}
 
@@ -25,7 +28,7 @@ public class PersonService {
 	}
 
 	public User loginUser(String login, String password) throws Exception {
-		User user = authenticateUser(login, password);
+		User user = this.personDAO.authenticateUser(login, password);
 		return user;
 	}
 }
