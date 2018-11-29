87289c2c8ce716c3f286a681612790f96c0e0cda
Gabriel <gabriel.stepien@inf.ufrgs.br>
2018-11-29 01:14:24 -0200

    camera controller

--- src/main/java/com/vrm/controller/CameraController.java
+++ src/main/java/com/vrm/controller/CameraController.java
@@ -3,21 +3,20 @@ package com.vrm.controller;
 import com.vrm.model.Person;
 import com.vrm.service.CameraService;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.core.util.SystemInfo;
 
 @RestController
 public class CameraController{
 
-    CameraService camService = new CameraService();
+    private CameraService camService = new CameraService();
     
     @PutMapping
-    public void teste(Person p, Integer camId) throws Exception {
-        
-        
-        camService.identifyPerson(p, camId);
-    
+    public void cameraIdentify(Person p, Integer camId) throws Exception {
+
+        this.camService.identifyPerson(p, camId);
     
     }
 
