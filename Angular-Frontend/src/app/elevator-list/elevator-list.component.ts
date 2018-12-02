b498fcc2a6a5c3ae8b416f35ed67b700236af909
Gabriel <gabriel.stepien@inf.ufrgs.br>
2018-12-02 03:01:42 -0200

    go

--- Angular-Frontend/src/app/elevator-list/elevator-list.component.ts
+++ Angular-Frontend/src/app/elevator-list/elevator-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-elevator-list',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ElevatorListComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private api: ApiService) { }
+  response = [];
   ngOnInit() {
+  }
+
+  moveElevators(){
+    console.log("Mooooving")
+    this.api.moveAllElevators().subscribe(res => {
+      this.response = res.split("#");
+  
+    });
   }
 
 }
